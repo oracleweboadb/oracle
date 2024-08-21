3f0625aca972d6d2dcf32e69ebf488cc36953667
qdnqn <adnn.selimovic@gmail.com>
2024-08-21 19:01:11 +0200

    Additional work

--- dev/oracle-main/weblogic-server/container-scripts/setUserOverrides.sh
+++ dev/oracle-main/weblogic-server/container-scripts/setUserOverrides.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ "${SERVER_NAME}" = "AdminServer" ]; then
+if [ "${HOSTNAME}" = "weblogic-admin" ]; then
     export USER_MEM_ARGS="-Xms1g -Xmx2g"
 fi
 
-if [ "${SERVER_NAME}" = "WLS_ORDS1" ]; then
+if [ "${wls_ords1}" = "managed-server1" ]; then
     export USER_MEM_ARGS="-Xms2g -Xmx6g"
 fi
 
-if [ "${SERVER_NAME}" = "WLS_ORDS2" ]; then
+if [ "${wls_ords2}" = "WLS_ORDS2" ]; then
     export USER_MEM_ARGS="-Xms2g -Xmx6g"
 fi
