a4603d4a377dc209b9ee0c078e439010146482b1
qdnqn <adnn.selimovic@gmail.com>
2024-08-21 16:26:44 +0200

    Additional work

--- dev/oracle-main/weblogic-server/container-scripts/deployAppToDomain.sh
+++ dev/oracle-main/weblogic-server/container-scripts/deployAppToDomain.sh
@@ -8,9 +8,9 @@ CURRENT_DIR=$(pwd)
 
 cd ${ORDS_HOME}
 
-echo $CUSTOM_SYS_PASSWORD > pass.txt
-echo $CUSTOM_SYS_PASSWORD >> pass.txt
-echo $CUSTOM_SYS_PASSWORD >> pass.txt
+echo "123456789aA123" > pass.txt
+echo "123456789aA123" >> pass.txt
+echo "123456789aA123" >> pass.txt
 
 java -jar $ORDS_HOME/ords.war install adb --wallet tls_wallet.zip --wallet-service-name myatp_medium --db-user ORDS_PUBLIC_USER2 --gateway-user ORDS_PLSQL_GATEWAY2 --admin-user admin --password-stdin < pass.txt
 java -jar $ORDS_HOME/ords.war --config $(pwd) war ords.war
